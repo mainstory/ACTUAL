308cc50ff06852d75c61cdab64f1825cfd24bfd1
mainstory <108094683+mainstory@users.noreply.github.com>
2023-04-01 17:21:39 +0300

    Add files via upload

--- js/base/burgerMenu.js
+++ js/base/burgerMenu.js
@@ -2,25 +2,28 @@
 
 function burgerMenu() {
 
-
    const open = document.querySelector('[data-menu-open]')
    const close = document.querySelector('[data-menu-close]')
 
    const shadow = document.querySelector('[data-menu-shadow]')
+   const body = document.querySelector('body')
 
    const menu = document.querySelector('[data-menu]')
 
    // открывается\закрывается
    // взаимодействуется тень
+   // взаимодействуется скролл
    
    open.addEventListener('click', (event) => {
       menu.classList.add('_active')
       shadow.classList.add('_active')
+      body.classList.add('_scroll-hidden')
    })
 
    close.addEventListener('click', (event) => {
       menu.classList.remove('_active')
       shadow.classList.remove('_active')
+      body.classList.remove('_scroll-hidden')
    })
 
    // закрывается по тени
@@ -28,6 +31,7 @@ function burgerMenu() {
    shadow.addEventListener('click', (event) => {
       menu.classList.remove('_active')
       shadow.classList.remove('_active')
+      body.classList.remove('_scroll-hidden')
    })
         
       
