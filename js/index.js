08de915c62ac4303fd7da20063668ea8210cd111
mainstory <108094683+mainstory@users.noreply.github.com>
2023-04-12 10:58:42 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,32 @@
+import {
+   burgerMenu,
+} from "./chunks/burgerMenu.js"
+import {
+   headerAnimation,
+} from "./chunks/headerAnimation.js"
+import {
+   spoilers,
+} from "./chunks/spoilers.js"
+import {
+   sliders,
+} from "./chunks/sliders.js"
+import {
+   numberAnimation,
+} from "./chunks/numberAnimation.js"
+import {
+   preloader,
+} from "./chunks/preloader.js"
+
+
+burgerMenu()
+headerAnimation()
+spoilers()
+sliders()
+numberAnimation()
+
+preloader()
+
+
+
+
+
